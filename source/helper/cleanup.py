d350df60d8e677059afd07bb7f6737f419cb69b6
darshanp228 <72122805+darshanp228@users.noreply.github.com>
2021-09-30 02:33:46 -0400

    Update cleanup.py
    Added delete_temp_files function

--- source/helper/cleanup.py
+++ source/helper/cleanup.py
@@ -1,3 +1,16 @@
 """
 @author: Scrivener
 """
+import os
+import shutil
+
+class Cleanup:
+    
+    def __init__(self):
+        pass
+            
+    def delete_temp_files(self):
+        try:
+            shutil.rmtree(os.getcwd() + "\\" + 'temp')
+        except OSError as e:
+            print(e.strerror)
