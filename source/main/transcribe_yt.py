959d5d1374177f2204620e046a0f2ef0fd40742c
darshanp228 <72122805+darshanp228@users.noreply.github.com>
2021-09-30 02:20:03 -0400

    Update transcribe_yt.py

--- source/main/transcribe_yt.py
+++ source/main/transcribe_yt.py
@@ -1,3 +1,24 @@
 """
 @author: Scrivener
 """
+
+from main.summarize import Summary
+import pytube
+import os
+from youtube_transcript_api import YouTubeTranscriptApi
+from main.transcribe import TranscribeVideo
+
+class TranscribeYtVideo:
+    def __init__(self,youtube_url):
+        self.youtube_url = youtube_url
+        self.yt_id = self.youtube_url.split('=')[1]
+        self.summary = ''
+        
+    def check_yt_cc(self):
+        transcript = None
+        try:
+            transcript_list = YouTubeTranscriptApi.list_transcripts(self.yt_id)
+            transcript = transcript_list.find_transcript(['en'])
+        except Exception as e:
+            print(e)
+        return transcript
