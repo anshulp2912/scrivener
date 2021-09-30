f0f4775ca0d2d0941c95b7a2584bcefa4430b402
rohan22shah <42119767+rohan22shah@users.noreply.github.com>
2021-09-30 19:57:14 +0530

    Update cleanup.py
    Added comments

--- source/helper/cleanup.py
+++ source/helper/cleanup.py
@@ -1,15 +1,31 @@
 """
 @author: Scrivener
 """
+
+# Import Libraries
 import os
 import shutil
 
 class Cleanup:
+    """
+    A class used to clean temporary files generated 
+    ...
+
+    Methods
+    -------
+    delete_temp_files:
+        function to delete temporary files that were created while generating
+        summary of youtube videos with closed captions.
+    """
     
     def __init__(self):
         pass
             
     def delete_temp_files(self):
+        """
+        function to delete temporary files that were created while generating
+        summary.
+        """
         try:
             shutil.rmtree(os.getcwd() + "\\" + 'temp')
         except OSError as e:
