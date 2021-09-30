fc07e9dfea3fbb9275ca158499afe838f76598d2
darshanp228 <72122805+darshanp228@users.noreply.github.com>
2021-09-30 11:00:33 -0400

    Update scrivener_user_interface.py
    Added Video Upload functionality

--- source/scrivener_user_interface.py
+++ source/scrivener_user_interface.py
@@ -4,10 +4,18 @@
 
 import streamlit as st
 import re
+import os
+from main.transcribe import TranscribeVideo
 from main.transcribe_yt import TranscribeYtVideo
 import secrets
+from glob import glob
 
 input_format = st.radio('Choose your input format', ['Youtube Link', 'Upload a Video'])
+
+def save_file(file):
+    with open(os.path.join(os.getcwd(), file.name), 'wb') as f:
+        f.write(file.getbuffer())
+    return 
 
 if input_format=='Youtube Link':
     youtube_link = st.text_input('Enter Youtube Link')
@@ -26,3 +34,19 @@ if input_format=='Youtube Link':
         
     elif youtube_link!='':
         st.error('Please enter a valid Youtube Link!')
+
+elif input_format=='Upload a Video':
+    file = st.file_uploader('Upload a video',type=['mp4'],accept_multiple_files=False)
+    if file is not None:
+
+        progress_lines = secrets.choice(['Hired Shakespeare to summarize your video', 'Taking advice from Charles Dickens to help you',
+                                        'Shakespeare is completing the assignment', 'Do not worry, Mark Twain is on it',
+                                        'Robert Frost is taking the right road to summarize your video'])
+        with st.spinner(progress_lines+' . . .'):
+            save_file(file)
+            transcribe_video = TranscribeVideo()
+            summary = transcribe_video.transcribe_video(os.path.join(os.getcwd(), file.name))
+        st.write(summary)
+    else:
+        for name in glob('*.mp4'):
+            os.remove(name)
